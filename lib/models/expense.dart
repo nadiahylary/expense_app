bd7b66280cf5b5efa812c31c9dad9a3a293a6c9e
Nadia HYLARY <nhylary12@gmail.com>
2023-05-12 19:00:53 +0100

    added data models

---
+++ lib/models/expense.dart
@@ -0,0 +1,16 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+enum Category {health, career, nutrition, business, spirituality, leisure}
+
+class Expense{
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+
+  Expense({required this.title, required this.amount, required this.date, required this.category}) : id = uuid.v4();
+
+}
